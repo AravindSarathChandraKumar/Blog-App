aa77adc7fa25db7498f3a56eaa90cc058b2f0647
AravindSarathChandraKumar <aravinds95@gmail.com>
2018-05-27 10:30:30 +0530

    blog post settings added

--- post/views.py
+++ post/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-   # return HttpResponse("This is Post Page")
-    return render(request,'post/index.html')
+    
+    posts = Post.objects.all()[:10]
+    context = {
+        'title':'Latest Posts',
+        'posts': posts
+    }
+    return render(request,'post/index.html',context)
+def details(request,id):
+    post= Post.objects.get(id=id)
+    context = {
+        'post':post
+    }
+    return render(request,'post/details.html',context)
